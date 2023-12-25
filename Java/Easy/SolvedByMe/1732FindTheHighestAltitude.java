caa7fd6a2dc7aa3d53963c89b476a811388cd55e
DhruvP23 <dhruvj1323@gmail.com>
2023-12-25 16:56:22 -0500

    Day 2

---
+++ Java/Easy/SolvedByMe/1732FindTheHighestAltitude.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int lastAlt = 0, highestAlt = 0, currAlt = 0;
+        for(int i = 0; i < gain.length; i++){
+            currAlt = lastAlt + gain[i];
+            lastAlt = currAlt;
+            highestAlt = Math.max(currAlt, highestAlt);
+        }
+        return highestAlt;
+    }
+}
